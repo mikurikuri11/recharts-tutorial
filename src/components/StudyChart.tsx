d59d720892a9903cfd3c3cebcd30f6000a12af56
mikurikuri1919 <mikuriyakenta003@gmail.com>
2024-01-29 19:08:34 +0900

    🎨 グラフの整形

--- src/components/StudyChart.tsx
+++ src/components/StudyChart.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   CartesianGrid,
   Legend,
@@ -7,30 +7,49 @@ import {
   Tooltip,
   XAxis,
   YAxis,
-} from 'recharts';
-import studyDataList from './studyData';
+} from "recharts";
+import studyDataList from "./studyData";
+
+const pStyle = {
+  color: 'blue',
+};
+
+const divStyle = {
+  background: 'linear-gradient(to right, #fff, #fff8)',
+  fontWeight: 'bold',
+  border: 'solid 2px blue',
+};
 
 const StudyChart = () => (
   <div className="container">
     <LineChart
-      width={700}
-      height={300}
+      width={1000}
+      height={600}
       data={studyDataList}
       margin={{
-        top: 5,
-        right: 5,
+        top: 10,
+        right: 40,
         left: 5,
-        bottom: 5,
+        bottom: 10,
       }}
     >
       <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="date" />
-      <YAxis dataKey="合計" />
-      <Line type="monotone" dataKey="合計" stroke="#1e17a7" />
-      <Line type="monotone" dataKey="達成数" stroke="#69ae31" />
-      <Line type="monotone" dataKey="達成率" stroke="#de499d" />
-      <Legend />
-      <Tooltip />
+      <XAxis
+        dataKey="date"
+        interval={0}
+        // angle={-30}
+        dx={-10}
+        dy={20}
+        tick={{
+          fontSize: 12,
+          fill: "#000",
+        }}
+      />
+      <YAxis dataKey="合計" tickCount={5} />
+      {/* <Line type="monotone" dataKey="達成数" stroke="#69ae31" strokeWidth={2} /> */}
+      <Line type="monotone" dataKey="達成率" stroke="#58b8e8" strokeWidth={2} />
+      <Legend verticalAlign="top" height={30} iconSize={20} iconType="plainline" />
+      <Tooltip contentStyle={divStyle} labelStyle={pStyle} />
     </LineChart>
   </div>
 );
